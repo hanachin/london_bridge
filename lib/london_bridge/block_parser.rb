71dd732c083328e3894085d87f3bda859918c9c2
Seiei Miyagi <hanachin@gmail.com>
2019-02-17 00:56:37 +0900

    Separate file

--- lib/london_bridge/block_parser.rb
+++ lib/london_bridge/block_parser.rb
@@ -1,87 +1,13 @@
+require 'london_bridge/block_parser/events'
+
 module LondonBridge
-  class BlockParser < Struct.new(:input)
-    class Event < Struct.new(:lineno, :source)
-      attr_accessor :options
-
-      def initialize(*args, **options)
-        super(*args)
-        @options = options
-      end
-    end
-    class StartEvent < Event; end
-    class EndEvent < Event; end
-    class InlineContentEvent < Event
-      def blank_line?
-        source.match(/^ *$/)
-      end
-    end
-
-    [StartEvent, EndEvent, InlineContentEvent].each do |klass|
-      n = klass.name.split('::').last
-      %w(ThematicBreak AtxHeading FencedCode IndentedCode BlankLines Paragraph BlockQuote UnOrderedList ListItem).each do |b|
-        const_set("#{b}#{n}", Class.new(klass))
-      end
-    end
-
-    AtxHeadingStartEvent.class_eval do
-      def depth
-        source.count(?#)
-      end
-    end
-
-    FencedCodeStartEvent.class_eval do
-      def info_string
-        @options.fetch(:info_string)
-      end
-
-      def language
-        info_string&.split(' ', 2)&.first
-      end
-    end
-
-    FencedCodeInlineContentEvent.class_eval do
-      def indent
-        @options.fetch(:indent)
-      end
-    end
-
-    FencedCodeInlineContentEvent.class_eval do
-      def indent
-        @options.fetch(:indent)
-      end
-    end
-
-    BlockQuoteInlineContentEvent.class_eval do
-      def child
-        @options.fetch(:child)
-      end
-    end
-
-
-    [ListItemStartEvent, ListItemEndEvent].each do |klass|
-      klass.class_eval do
-        def list_indent
-          @options.fetch(:list_indent)
-        end
-
-        def tight?
-          @options.fetch(:tight, false)
-        end
-      end
-    end
-
-    ListItemInlineContentEvent.class_eval do
-      def child
-        @options.fetch(:child)
-      end
-
-      def tight?
-        @options.fetch(:tight, false)
-      end
+  class BlockParser
+    def initialize(input)
+      @input = input
     end
 
     def each
-      input = self.input.each.with_index
+      input = @input.each.with_index
       last_lineno = 0
       @ul_continue = false
       loop do
